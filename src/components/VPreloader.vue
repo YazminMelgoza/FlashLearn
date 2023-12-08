637831315d9537e6c8a83156220b89f90e0d1e96
YazminMelgoza <yazmin.melgozazamarripa@gmail.com>
2023-12-07 19:57:31 -0600

    merged

---
+++ src/components/VPreloader.vue
@@ -0,0 +1,10 @@
+<script setup></script>
+<template>
+  <div class="w-full h-full items-center justify-center flex">
+    <img
+      :class="classes"
+      src="https://camaradecomerciogdl1.com/camaraaccess/static/img/loading2.gif"
+      alt=""
+    />
+  </div>
+</template>
