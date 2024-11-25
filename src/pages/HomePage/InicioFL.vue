337340f94cf05b00f8e53da4c360ce446f8d4df9
Alejandro Sauqué Pérez <alejandro.sauqueprz@uanl.edu.mx>
2024-11-25 17:59:23 -0600

    Se elimina estado de carga innecesario

--- src/pages/HomePage/InicioFL.vue
+++ src/pages/HomePage/InicioFL.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="userStore.isLoaded" class="flex flex-col h-full">
+  <div class="flex flex-col h-full">
     <HeaderTop
       :title="`Buen día, ${username}!`"
       :subtitle="`Has estudiado por ${streakDuration} día${
@@ -10,13 +10,6 @@
       <SetReciente />
       <MiProgreso />
     </div>
-  </div>
-  <div v-else class="w-full h-full items-center justify-center flex">
-    <img
-      class=""
-      src="https://camaradecomerciogdl1.com/camaraaccess/static/img/loading2.gif"
-      alt=""
-    />
   </div>
 </template>
 
